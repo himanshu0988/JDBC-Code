dee026da5b8996e5e760fa14959d0e68bd9f4e86
HIMANSHU <HIMANSHU@DESKTOP-J1M9KRJ>
2024-03-27 15:28:45 +0530

    InsertApp Using JDBC

--- JDBC/src/in/ineuron/jdbc/InsertApp.java
+++ JDBC/src/in/ineuron/jdbc/InsertApp.java
@@ -2,51 +2,37 @@ package in.ineuron.jdbc;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-
-
-public class Jdbc3 {
+public class InsertApp {
 
 	public static void main(String[] args) {
-		
 		Connection connection=null;
 		Statement statement=null;
-		ResultSet resultset=null;
 		try {
-			
-			
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			
-			String url="jdbc:mysql://localhost:3306/detail";
+			String url="jdbc:mysql:///detail";
 			String user="root";
 			String password="9852451029";
+			
 			connection=DriverManager.getConnection(url,user,password);
 			
 			statement=connection.createStatement();
-			String Query="Select * from studnet";
-			resultset=statement.executeQuery(Query);
 			
-			while(resultset.next())
-			{
-				Integer id=resultset.getInt(1);
-				String name=resultset.getString(2);
-				Integer Age=resultset.getInt(3);
-				
-				System.out.println(id+"\t"+name+"\t"+Age);
-			}
+			String Query=" insert into studnet(sid,sname,sage)values(12,'juhi',24)";
+			int rowUdate=statement.executeUpdate(Query);
 			
-			} catch (ClassNotFoundException e) {
+			System.out.println("total row updated is "+rowUdate);
 			
-			e.printStackTrace();
-		}
-		catch (SQLException e) {
+			
+		} catch (ClassNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}
-		finally
+		}catch(SQLException se)
+		{
+			se.printStackTrace();
+		}finally
 		{
 			if(connection !=null)
 			{
@@ -57,16 +43,8 @@ public class Jdbc3 {
 					e.printStackTrace();
 				}
 			}
-			if(statement !=null)
-			{
-				try {
-					statement.close();
-				} catch (SQLException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
 		}
+		
 
 	}
 
